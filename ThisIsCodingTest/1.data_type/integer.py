5720331e48c4b4dfb94f632b82f6e2760f65b9da
kwakwoohyun010@gmail.com <kwakwoohyun010@gmail.com>
2021-07-08 00:19:46 +0900

    feat: Add this is coding test study & first day -> data type 이것이 코딩테스트다. 스터디

---
+++ ThisIsCodingTest/1.data_type/integer.py
@@ -0,0 +1,16 @@
+# 양의 정수
+a = 1000
+print(a)
+a = a + 5
+print(a)
+print(type(a))
+
+# 음의 정수
+a = -7
+print(a)
+print(type(a))
+
+# 0
+a = 0
+print(a)
+print(type(a))
---
+++ ThisIsCodingTest/1.data_type/time.py
@@ -0,0 +1,14 @@
+import time
+
+# 시작시간
+start_time = time.time()
+
+print("hihi")
+print("시간측정")
+print("hihi")
+
+# 끝난시간
+end_time = time.time()
+
+# 시간 측정
+print(end_time-start_time)
